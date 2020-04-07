a1c7131d8c19aa83611ef1a28882207f556e5027
Hillel Saal <remi.saal@gmail.com>
2020-04-07 10:22:11 +0200

    Added StoreRepository

---
+++ Data/Repositories/StoreRepository.cs
@@ -0,0 +1,56 @@
+﻿using IceCreamDesktop.Core.Entities;
+using IceCreamDesktop.Core.Failures;
+using IceCreamDesktop.Domain.Interfaces;
+using Monad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamDesktop.Data.Repositories
+{
+	public class StoreRepository : IStoreRepository
+	{
+		private readonly KioskContext Kiosk;
+
+		public StoreRepository(KioskContext kiosk)
+		{
+			Kiosk = kiosk;
+		}
+
+		public async Task<Either<Failure, Store>> AddStore(Store store)
+		{
+			try
+			{
+				Kiosk.Stores.Add(store);
+				await Kiosk.SaveChangesAsync();
+				return () => store;
+			}
+			catch (Exception)
+			{
+				return () => new DataAccessFailure("An error has occured");
+			}
+		}
+
+		public Task<List<Store>> GetAllStores()
+		{
+			if (Kiosk.Stores.Any())
+				return Task.FromResult(Kiosk.Stores.ToList());
+			return Task.FromResult(new List<Store>());
+		}
+
+		public async Task<Option<Failure>> RemoveStore(int id)
+		{
+			var matches = Kiosk.Stores.Where(store => id == store.Id).ToList();
+
+			if (!matches.Any())
+				return () => new DataAccessFailure("You can't delete this store!");
+
+			Kiosk.Stores.Remove(matches.First());
+			await Kiosk.SaveChangesAsync();
+
+			return Option.Nothing<Failure>();
+		}
+	}
+}
--- DomainTests/Usecases/IceCream/GetAllIceCreamTests.cs
+++ DomainTests/Usecases/IceCream/GetAllIceCreamTests.cs
@@ -1,7 +1,5 @@
 ﻿using IceCreamDesktop.Core.Entities;
-using IceCreamDesktop.Core.Failures;
 using IceCreamDesktop.Domain.Interfaces;
-using Monad;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -12,49 +10,49 @@ using Xunit.Sdk;
 
 namespace IceCreamDesktop.Domain.Usecases.Tests
 {
-    [Collection("IceCreamUseCases")]
-    public class GetAllIceCreamTests
-    {
-        public static readonly IIceCreamRepository TRepository = Mock.Create<IIceCreamRepository>();
+	[Collection("IceCreamUseCases")]
+	public class GetAllIceCreamTests
+	{
+		public static readonly IIceCreamRepository TRepository = Mock.Create<IIceCreamRepository>();
 
-        public static readonly List<IceCream> TIceCreams = new List<IceCream> {
-            new IceCream {
-                Name = "IceCream1",
-                Brand = "IceCream1Brand",
-                ImageUrl = "https://www.osem.co.il/tm-content/uploads/2014/12/crunchVanilla-308x308.png"
-            },
-            new IceCream {
-                Name = "IceCream2",
-                Brand = "IceCream2Brand",
-                ImageUrl = "https://www.osem.co.il/tm-content/uploads/2014/12/crunchVanilla-308x308.png"
-            },
-        };
+		public static readonly List<IceCream> TIceCreams = new List<IceCream> {
+			new IceCream {
+				Name = "IceCream1",
+				Brand = "IceCream1Brand",
+				ImageUrl = "https://www.osem.co.il/tm-content/uploads/2014/12/crunchVanilla-308x308.png"
+			},
+			new IceCream {
+				Name = "IceCream2",
+				Brand = "IceCream2Brand",
+				ImageUrl = "https://www.osem.co.il/tm-content/uploads/2014/12/crunchVanilla-308x308.png"
+			},
+		};
 
-        [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-        private class ClearMockAfter : BeforeAfterTestAttribute
-        {
-            public override void After(MethodInfo methodUnderTest)
-            {
-                Mock.Reset();
-            }
-        }
+		[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+		private class ClearMockAfter : BeforeAfterTestAttribute
+		{
+			public override void After(MethodInfo methodUnderTest)
+			{
+				Mock.Reset();
+			}
+		}
 
-        [ClearMockAfter()]
-        [Trait("Method", "Call")]
-        [Fact(DisplayName = "Should return a list of ice creams when the repository is successful")]
-        public async Task GetAllIceCreamTest1()
-        {
-            // Arrange
-            Mock.Arrange(() => TRepository.GetAllIceCreams()).TaskResult(TIceCreams);
-            GetAllIceCreams getAllIceCreams = new GetAllIceCreams(TRepository);
-            GetAllIceCreamsArgs args = new GetAllIceCreamsArgs();
+		[ClearMockAfter()]
+		[Trait("Method", "Call")]
+		[Fact(DisplayName = "Should return a list of ice creams when the repository is successful")]
+		public async Task GetAllIceCreamTest1()
+		{
+			// Arrange
+			Mock.Arrange(() => TRepository.GetAllIceCreams()).TaskResult(TIceCreams);
+			GetAllIceCreams getAllIceCreams = new GetAllIceCreams(TRepository);
+			GetAllIceCreamsArgs args = new GetAllIceCreamsArgs();
 
-            // Act
-            var result = await getAllIceCreams.Call(args);
+			// Act
+			var result = await getAllIceCreams.Call(args);
 
-            // Assert
-            Assert.NotEmpty(result);
-            Assert.Equal(TIceCreams.Count, result.Count);
-        }
-    }
+			// Assert
+			Assert.NotEmpty(result);
+			Assert.Equal(TIceCreams.Count, result.Count);
+		}
+	}
 }
